921159a47525021f332cbc3842cc7ae56ecca461
Ian Yang <me@iany.me>
2011-12-30 20:36:41 +0800

    match yard doc only in comment

--- doxymacs-yard.el
+++ doxymacs-yard.el
@@ -20,6 +20,26 @@
 ;;   (add-hook 'ruby-mode-hook 'doxymacs-yard-font-lock)
 ;;
 
+
+;;; Functions copied from http://www.emacswiki.org/emacs/fic-mode.el
+(defun doxymacs-yard--in-doc/comment-region (pos)
+  (memq (get-char-property pos 'face)
+	(list font-lock-doc-face font-lock-comment-face)))
+(defun doxymacs-yard--search-regexp (regexp limit)
+  (let ((match-data-to-set nil)
+        found)
+    (save-match-data
+      (while (and (null match-data-to-set)
+                  (re-search-forward regexp limit t))
+        (if (and (doxymacs-yard--in-doc/comment-region (match-beginning 0))
+                 (doxymacs-yard--in-doc/comment-region (match-end 0)))
+            (setq match-data-to-set (match-data)))))
+    (when match-data-to-set
+      (set-match-data match-data-to-set)
+      (princ (match-string 0))
+      (goto-char (match-end 0))
+      t)))
+
 (defun doxymacs-yard--make-keywords-regexp(&rest keywords)
   (concat
    "\\([@\\\\]\\(?:"
@@ -40,74 +60,96 @@
 (defconst doxymacs-yard--variable-type-regexp "\\(\\[[^]]+\\]\\)")
 (defconst doxymacs-yard--variable-name-regexp "\\([a-zA-Z0-9_]+\\)")
 
-(defconst doxymacs-yard-keywords
-  (list
-   (list
-    ;; One shot keywords that take no arguments
+;; One shot keywords that take no arguments
+(defun doxymacs-yard--search-keywords (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--make-keywords-regexp
+    "abstract"
+    "api"
+    "author"
+    "deprecated"
+    "example"
+    "note"
+    "private"
+    "scope"
+    "see"
+    "since"
+    "todo"
+    "version"
+    "visibility")
+   limit))
+;; Keywords that take variable type and name as arguments
+(defun doxymacs-yard--search-keywords-with-type-and-name (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--concat-optional-regexp-with-whitespace
     (doxymacs-yard--make-keywords-regexp
-     "abstract"
-     "api"
-     "author"
-     "deprecated"
-     "example"
-     "note"
-     "private"
-     "scope"
-     "see"
-     "since"
-     "todo"
-     "version"
-     "visibility")
-    '(0 font-lock-keyword-face prepend))
+     "attr\\(?:_reader\\|_wirter\\|ibute\\)?"
+     "macro"
+     "param"
+     "yieldparam")
+    doxymacs-yard--variable-type-regexp
+    doxymacs-yard--variable-name-regexp)
+   limit))
+;; Keywords that take function as argument
+(defun doxymacs-yard--search-keywords-with-type-and-function (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--concat-optional-regexp-with-whitespace
+    (doxymacs-yard--make-keywords-regexp
+     "method"
+     "overload")
+    "\\(\sw+\\)")
+   limit))
+;; Keywords that take variable name, then type and then variable name as arguments
+(defun doxymacs-yard--search-keywords-with-name-type-and-name (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--concat-optional-regexp-with-whitespace
+    (doxymacs-yard--make-keywords-regexp "option")
+    doxymacs-yard--variable-name-regexp
+    doxymacs-yard--variable-type-regexp
+    doxymacs-yard--variable-name-regexp)
+   limit))
+;; Keywords that take optional variable type
+(defun doxymacs-yard--search-keywords-with-type (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--concat-optional-regexp-with-whitespace
+    (doxymacs-yard--make-keywords-regexp
+     "raise"
+     "return"
+     "yieldreturn")
+    doxymacs-yard--variable-type-regexp)
+   limit))
+;; Keywords that take list of variable names
+(defun doxymacs-yard--search-keywords-with-name (limit)
+  (doxymacs-yard--search-regexp
+   (doxymacs-yard--concat-optional-regexp-with-whitespace
+    (doxymacs-yard--make-keywords-regexp "yield")
+    doxymacs-yard--variable-type-regexp)
+   limit))
+
+(setq doxymacs-yard-keywords
+  (list
+   (list 'doxymacs-yard--search-keywords '(0 font-lock-keyword-face prepend))
    (list
-    ;; Keywords that take variable type and name as arguments
-    (doxymacs-yard--concat-optional-regexp-with-whitespace
-     (doxymacs-yard--make-keywords-regexp
-      "attr\\(?:_reader\\|_wirter\\|ibute\\)?"
-      "macro"
-      "param"
-      "yieldparam")
-     doxymacs-yard--variable-type-regexp
-     doxymacs-yard--variable-name-regexp)
+    'doxymacs-yard--search-keywords-with-type-and-name
     '(1 font-lock-keyword-face prepend)
     '(2 font-lock-type-face prepend t)
     '(3 font-lock-variable-name-face prepend t))
    (list
-    ;; Keywords that take function as argument
-    (doxymacs-yard--concat-optional-regexp-with-whitespace
-     (doxymacs-yard--make-keywords-regexp
-      "method"
-      "overload")
-     "\\(\sw+\\)")
+    'doxymacs-yard--search-keywords-with-type-and-function
     '(1 font-lock-keyword-face prepend)
     '(2 font-lock-function-name-face prepend t))
    (list
-    ;; Keywords that take variable name, then type and then variable name as arguments
-    (doxymacs-yard--concat-optional-regexp-with-whitespace
-     (doxymacs-yard--make-keywords-regexp "option")
-     doxymacs-yard--variable-name-regexp
-     doxymacs-yard--variable-type-regexp
-     doxymacs-yard--variable-name-regexp)
+    'doxymacs-yard--search-keywords-with-name-type-and-name
     '(1 font-lock-keyword-face prepend)
     '(2 font-lock-variable-name-face prepend t)
     '(3 font-lock-type-face prepend t)
     '(4 font-lock-variable-name-face prepend t))
    (list
-    ;; Keywords that take optional variable type
-    (doxymacs-yard--concat-optional-regexp-with-whitespace
-     (doxymacs-yard--make-keywords-regexp
-      "raise"
-      "return"
-      "yieldreturn"
-      )
-     doxymacs-yard--variable-type-regexp)
+    'doxymacs-yard--search-keywords-with-type
     '(1 font-lock-keyword-face prepend)
     '(2 font-lock-type-face prepend t))
    (list
-    ;; Keywords that take list of variable names
-    (doxymacs-yard--concat-optional-regexp-with-whitespace
-     (doxymacs-yard--make-keywords-regexp "yield")
-     doxymacs-yard--variable-type-regexp)
+    'doxymacs-yard--search-keywords-with-name
     '(1 font-lock-keyword-face prepend)
     '(2 font-lock-variable-name-face prepend t))))
 
